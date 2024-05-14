68baf5bf94a2e92c381953ea4fd49db8c9b24375
WilliamDevin23 <wdevinsp@gmail.com>
2024-05-14 12:09:58 +0700

    Update layout and add live data feature

--- dashboard.py
+++ dashboard.py
@@ -1,44 +1,49 @@
 import streamlit as st
 from utils import *
+import time
 
 def main() :
+    
+    #Session states
     if "chart_type" not in st.session_state : st.session_state.chart_type = "Candlestick"
     if "period_filter" not in st.session_state : st.session_state.period_filter = "1d"
     if "interval_filter" not in st.session_state : st.session_state.interval_filter = "5m"
     if "code" not in st.session_state : st.session_state.code = "IHSG"
-    
-    st.header("Indonesian Stock Exchange Dashboard")
-    col1, col2, col3 = st.columns([0.4, 0.3, 0.3])
 
     def new_code():
         if st.session_state.new_code[:4] != st.session_state.code:
             st.session_state.code = st.session_state.new_code[:4]
-
-    with col1:
-        option = st.selectbox("IDX Stocks",
+            
+    st.header("Indonesian Stock Exchange Dashboard")
+    option = st.selectbox("IDX Stocks",
                 get_idx(), key="new_code",
                 on_change=new_code)
-        code = option[:4]
-        name = option[5:]
-    
-    with col2:
-        st.subheader(code)
-        st.write(name)
-    
-    with col3:
+    code = option[:4]
+    name = option[5:]
+
+    placeholder = st.empty()
+
+    with placeholder.container():
+        col1, col2 = st.columns([0.6, 0.4])
+        
+        with col1:
+            st.subheader(code)
+            st.write(name)
+        
         if st.session_state.code == "IHSG":
-            stock_data, datebreaks = get_stock("^JKSE", st.session_state.period_filter,
-                                                st.session_state.interval_filter)
+                stock_data, datebreaks = get_stock("^JKSE", st.session_state.period_filter,
+                                                    st.session_state.interval_filter)
         else :
             stock_data, datebreaks = get_stock(st.session_state.code+".JK", st.session_state.period_filter,
                                                 st.session_state.interval_filter)
         stock_metric = get_metric(stock_data)
-        st.metric(label="Close Price",
+
+        col2.metric(label="Close Price",
                 value="Rp {0}".format(stock_metric['Close']),
                 delta="{0} ({1} %)".format(stock_metric['Diff'], stock_metric['Percent']))
 
-    fig = make_graph(stock_data, datebreaks, st.session_state.interval_filter, st.session_state.chart_type)
-    st.write(fig)
+        fig = make_graph(stock_data, datebreaks, st.session_state.interval_filter, st.session_state.chart_type)
+        st.write(fig)
 
     filter1, filter2, filter3 = st.columns(3)
     time_dict = {"5m":"5 Minutes", "1h":"1 Hour",
@@ -85,6 +90,28 @@ def main() :
         st.selectbox("Interval", intervals, format_func=format_func,
             key="new_interval", on_change=update_interval)
 
+    while True:
+        with placeholder.container():
+            col1, col2 = st.columns([0.6, 0.4])
+            
+            with col1:
+                st.subheader(code)
+                st.write(name)
+            
+            if st.session_state.code == "IHSG":
+                    stock_data, datebreaks = get_stock("^JKSE", st.session_state.period_filter,
+                                                        st.session_state.interval_filter)
+            else :
+                stock_data, datebreaks = get_stock(st.session_state.code+".JK", st.session_state.period_filter,
+                                                    st.session_state.interval_filter)
+            stock_metric = get_metric(stock_data)
 
+            col2.metric(label="Close Price",
+                    value="Rp {0}".format(stock_metric['Close']),
+                    delta="{0} ({1} %)".format(stock_metric['Diff'], stock_metric['Percent']))
+
+            fig = make_graph(stock_data, datebreaks, st.session_state.interval_filter, st.session_state.chart_type)
+            st.write(fig)
+        time.sleep(10)
 if __name__ == "__main__":
     main()
