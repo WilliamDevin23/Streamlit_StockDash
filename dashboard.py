27786ec63152ee182782a4ba25e10ccd4a013949
WilliamDevin23 <wdevinsp@gmail.com>
2024-06-22 13:51:17 +0700

    Add day constraint, layout modif

--- dashboard.py
+++ dashboard.py
@@ -5,10 +5,13 @@ import time
 from datetime import datetime
 import pytz
 
+st.set_page_config(layout='wide')
+
 def main() :
     
     jkt_tz = pytz.timezone('Asia/Jakarta')
     jkt_hour = int(datetime.now(jkt_tz).strftime("%H"))
+    jkt_day = datetime.now(jkt_tz).strftime("%A")
     
     #Session states
     if "chart_type" not in st.session_state : st.session_state.chart_type = "Candlestick"
@@ -30,7 +33,7 @@ def main() :
     scaled_data = minmax_data(daily_data, scaler)
     forecast = int(model_forecast(scaler, model, scaled_data[-51:-1]))
 
-    st.header("Indonesian Stock Exchange Dashboard")
+    st.markdown("<h1 style='text-align: center'>Indonesian Stock Exchange Dashboard</h1>", unsafe_allow_html=True)
     option = st.selectbox("IDX Stocks",
                           get_idx(), key="new_code",
                           on_change=new_code)
@@ -42,36 +45,43 @@ def main() :
     def update_data() :
         
         global stock_data
+        global stock_metric
         
         with placeholder.container():
-            col1, col2, col3 = st.columns(3)
-            
-            with col1:
-                st.subheader(code)
-                st.write(name)
-            
-            if st.session_state.code == "IHSG":
-                    stock_data, datebreaks = get_stock("^JKSE", st.session_state.period_filter,
-                                                       st.session_state.interval_filter)
-            else :
-                stock_data, datebreaks = get_stock(st.session_state.code+".JK", st.session_state.period_filter,
-                                                   st.session_state.interval_filter)
-            stock_metric = get_metric(stock_data, forecast)
+            graph, pred = st.columns([0.8, 0.2], gap='small')
+        
+            with graph :
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.subheader(code)
+                    st.write(name)
+                
+                if st.session_state.code == "IHSG":
+                        stock_data, datebreaks = get_stock("^JKSE", st.session_state.period_filter,
+                                                        st.session_state.interval_filter)
+                else :
+                    stock_data, datebreaks = get_stock(st.session_state.code+".JK", st.session_state.period_filter,
+                                                    st.session_state.interval_filter)
 
-            col2.metric(label="Close Price",
-                        value="Rp {0}".format(stock_metric['Close']),
-                        delta="{0} ({1} %)".format(stock_metric['Diff'], stock_metric['Percent']))
-            
-            col3.metric(label="Predicted Close Price Today",
-                        value="Rp {0}".format(forecast),
-                        delta="{0} ({1} %)".format(stock_metric['Diff Forecast'], stock_metric['Percent Forecast']))
+                stock_metric = get_metric(stock_data, forecast)
+                
+                col2.metric(label="Close Price",
+                            value="Rp {0}".format(stock_metric['Close']),
+                            delta="{0} ({1} %)".format(stock_metric['Diff'], stock_metric['Percent']))
 
-            fig = make_graph(stock_data, datebreaks, st.session_state.interval_filter, st.session_state.chart_type)
-            st.write(fig)
+                fig = make_graph(stock_data, datebreaks, st.session_state.interval_filter, st.session_state.chart_type)
+                st.plotly_chart(fig, use_container_width=True)
+            
+            with pred :
+                st.subheader("Predictions")
+                st.metric(label="Predicted Close Price Today",
+                            value="Rp {0}".format(forecast),
+                            delta="{0} ({1} %)".format(stock_metric['Diff Forecast'], stock_metric['Percent Forecast']))
     
     update_data()
-
+    
     filter1, filter2, filter3 = st.columns(3)
+    
     time_dict = {"5m":"5 Minutes", "1h":"1 Hour",
                  "1d":"1 Day", "1wk":"1 Week",
                  "1mo":"1 Month", "1y":"1 Year", "1mo":"1 Month",
@@ -119,8 +129,7 @@ def main() :
     st.subheader("Download the CSV")
     st.write(stock_data)
     
-    while jkt_hour >= 9 and jkt_hour <= 16 :
+    while jkt_hour >= 9 and jkt_hour <= 16 and not (jkt_day == "Saturday" or jkt_day == "Sunday") :
         update_data()
-        time.sleep(30)
 if __name__ == "__main__":
     main()
