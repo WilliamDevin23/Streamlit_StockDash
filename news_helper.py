f856a9145a17a30cb1ea1a700f3cd59db83713d2
WilliamDevin23 <wdevinsp@gmail.com>
2024-07-14 21:05:58 +0700

    Workflows for news update

---
+++ news_helper.py
@@ -0,0 +1,36 @@
+import requests
+import pandas as pd
+import os
+
+def get_articles(code) :
+    url = "https://news-api14.p.rapidapi.com/v2/search/articles"
+    headers = {
+    "x-rapidapi-key": os.getenv('RAPIDAPI_KEY'),
+    "x-rapidapi-host": "news-api14.p.rapidapi.com"
+    }
+    values = []
+    querystring = {"query":code,"language":"id","limit":"10"}
+    response = requests.get(url, headers=headers, params=querystring)
+    for article in response.json()['data'] :
+        values.append(code)
+        values.append(article["title"])
+        values.append(article["excerpt"])
+        values.append(article["url"])
+        values.append(article["publisher"]['name'])
+        values.append(article["date"])
+  return values
+  
+def to_dataframe(values) :
+    df = pd.DataFrame(values, columns=["Code", "Title", "Description", "URL", "Publisher", "Date"])
+    return df
+  
+def change_date_format(articles_df) :
+    articles_df["Date"] = pd.to_datetime(articles_df["Date"])
+    articles_df["Date"] = articles_df["Date"].apply(lambda x: x.strftime("%Y-%m-%d"))
+    return articles_df
+
+def fill_publisher(row):
+    if "https" in row["Publisher"] or row["Publisher"] == '':
+        return row["URL"].split("/")[2].replace("www.", "")
+    else :
+        return row["Publisher"]
---
+++ update_news.py
@@ -0,0 +1,30 @@
+import concurrent.futures
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+import psycopg2
+from news_helper import *
+
+conn_str = os.getenv('NEON_CONN_STR')
+conn = psycopg2.connect(conn_str)
+cur = conn.cursor()
+cur.execute("SELECT code FROM lq45;")
+codes = cur.fetchall()
+codes = sorted([c[0] for c in codes] + ["IHSG", "LQ45"])
+cur.close()
+conn.close()
+
+with concurrent.futures.ThreadPoolExecutor() as executor :
+	futures = []
+    data = []
+	for code in codes :
+		futures.append(executor.submit(get_articles, code=code))
+    for future in concurrent.futures.as_completed(futures) :
+        data.append(future.result())
+
+df = to_dataframe(data)
+df = change_date_format(df)
+df["Publisher"] = df.apply(fill_publisher, axis=1)
+
+engine = create_engine(conn_str)
+df.to_sql('news', engine, if_exists='replace', index=False)
