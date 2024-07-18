2c72a16e0f5ed330034d0673844e6e87b59686ca
WilliamDevin23 <wdevinsp@gmail.com>
2024-07-18 08:43:51 +0700

    Fix typo, use empty() after timer end

--- dashboard.py
+++ dashboard.py
@@ -41,7 +41,7 @@ def main() :
     if "ma_disable" not in st.session_state : st.session_state.ma_disable = True
     if "h_disable" not in st.session_state : st.session_state.h_disable = True
     if "realtime" not in st.session_state : st.session_state.realtime = True
-    if "timer_placeholder" not in st.session_state = st.session_state.timer_placeholder = st.empty()
+    if "timer_placeholder" not in st.session_state : st.session_state.timer_placeholder = st.empty()
     
     # Function to handle code choice
     def new_code():
@@ -423,5 +423,5 @@ if __name__ == "__main__":
         timer(st.session_state.timer_placeholder)
         market_close = ((8 <= hour < 9) or (minute < 15 and hour == 9)) and (day != "Saturday" or day != "Sunday")
     
-    st.session_state.timer_placeholder = None
+    st.session_state.timer_placeholder.empty()
     main()
