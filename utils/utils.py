2a98768869a0b1892df1b29c7eb4da6ba9d3ef69
WilliamDevin23 <wdevinsp@gmail.com>
2024-07-26 15:55:19 +0700

    Modify shape color to blue

--- utils/utils.py
+++ utils/utils.py
@@ -73,8 +73,8 @@ def make_graph(data, datebreaks, interval, chart_type, ma_arr, colors, stochasti
                       modebar_add = ['drawline', 'drawrect', 'eraseshape'],
                       modebar_remove = ['lasso2d', 'select2d', 'zoomIn2d', 'zoomOut2d'],
                       legend=dict(yanchor='top', xanchor='left', x=0.01, y=0.98),
-                      newshape=dict(line_color='white',
-                                fillcolor='white',
+                      newshape=dict(line_color='blue',
+                                fillcolor='cyan',
                                 opacity=0.28),
                       hoverlabel_font=dict(color="white"))
     fig.update_xaxes(rangebreaks=[{"values":datebreaks, "dvalue": dval*60*1000}])
